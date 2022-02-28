9cabc77352658e94b7c4a3ac433bb22f5e7464bf
Gabriel-Prisco <gabriel.psilva@bandtec.com.br>
2022-02-27 22:22:09 -0300

    feat: scripts de banco de dados criados

---
+++ banco_de_dados/script_hibisco_database_mysql.sql
@@ -0,0 +1,44 @@
+create database Hibisco;
+
+use Hibisco;
+
+create table tb_address_data (
+    id_address int not null primary key auto_increment,
+    address varchar(150),
+    neighborhood varchar(100),
+    city varchar(60),
+    uf char(2), 
+    cep varchar(8)
+);
+
+create table tb_donator (
+	id_donator int not null primary key auto_increment,
+    name_donator varchar(255) not null,
+    cpf char(14) not null,
+    blood_type char(3) not null,
+    email varchar(100) not null, 
+    password varchar(100) not null,
+    phone1 varchar(14) not null,
+    phone2 varchar(14),
+    fk_address int not null, foreign key (fk_address) references tb_address_data (id_address)
+);
+
+create table tb_hospital (
+    id_hospital int not null primary key auto_increment,
+    name_hospital varchar(255),
+    email varchar(100),
+    password varchar(100),
+    fk_address int not null, foreign key (fk_address) references tb_address_data (id_address)
+);
+
+create table tb_appointment (
+    id_appointment int not null primary key auto_increment,
+    dh_scheduling datetime default current_timestamp,
+    is_accepted BIT not null default 0,
+    fk_donator int not null, 
+    fk_hospital int not null, 
+    foreign key (fk_donator) references tb_donator (id_donator),
+    foreign key (fk_hospital) references tb_hospital (id_hospital)
+);
+
+
